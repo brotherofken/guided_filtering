c9870c9227db7e6550dfe03a9d90ec9e0360acc4
Rasim Akhunzyanov <brotherofken@gmail.com>
2020-12-06 21:29:55 +0300

    Renae functions, minor refactoring.

--- README.md
+++ README.md
@@ -6,3 +6,55 @@ mkdir build && cd build
 cmake -DHalide_DIR=$(Insert halide directory) ..
 make -j$(expr $(nproc) \+ 1)
 ```
+
+### Algorithm
+```dot
+digraph G {
+  rankdir = TB;
+
+  i -> mean_i;
+  p -> mean_p;
+
+  i -> channel_corr_i;
+  
+  i -> corr_ip;
+  p -> corr_ip;
+  
+  mean_i -> var_i;
+  channel_corr_i -> var_i;
+  
+  mean_i -> cov_ip;
+  mean_p -> cov_ip;
+  corr_ip -> cov_ip;
+  
+  cov_ip -> a;
+  var_i -> a;
+  
+  a -> b;
+  mean_i -> b;
+  mean_p -> b;
+  
+  a -> mean_a;
+  b -> mean_b;
+  
+  mean_a -> q;
+  mean_b -> q;
+  i -> q;
+
+  i [shape=rectangle, width=1, label=<guidance_i>];
+  p [shape=rectangle, width=1, label=<input_p>];
+  q [shape=rectangle, width=2, label=<q <br/> mean_a .* i + mean_b>];
+
+  mean_i [label=<mean_i>]
+  mean_p [label=<mean_p>]
+  mean_a [label=<mean_a>]
+  mean_b [label=<mean_b>]
+
+  corr_ip [label=<corr_ip <br/> = boxblur(I .∗ p)>]
+  channel_corr_i [label=<channel_corr_i <br/> = boxblur(I .∗ I)>]
+  var_i [label=<var_i <br/> = channel_corr_i− mean_i .∗ mean_i>]
+  cov_ip [label=<cov_ip <br/> = corr_ip − mean_i .∗ mean_p>]
+  a [label=<a <br/> = cov_ip ./ var_i>]
+  b [label=<b <br/> = mean_p - a .* mean_i>]
+}
+```
--- src/color_guided_image_filter_generator.cpp
+++ src/color_guided_image_filter_generator.cpp
@@ -10,7 +10,7 @@ namespace {
 
     class ColorGuidedPipeline : public Halide::Generator<ColorGuidedPipeline> {
 
-        Func& calc_channel_cov(Func& dest, const Func& f1, const Func& f2, const int channels) {
+        Func& calc_channel_corr(Func& dest, const Func& f1, const Func& f2, const int channels) {
             assert(f1.dimensions() == f2.dimensions());
             static Buffer<int> i = []() {
                 Buffer<int> buf(6, "channel_lookup_i");
@@ -28,8 +28,9 @@ namespace {
         }
 
     public:
-        Input<Halide::Buffer<uint8_t>> input{"input", 2};  // input image single channel
-        Input<Halide::Buffer<uint8_t>> guidance{"guidance", 3};  // 3 channels
+        Input<Halide::Buffer<uint8_t>> input_p{"input_p", 2};  // input_p image single channel
+        Input<Halide::Buffer<uint8_t>> guidance_i{"guidance_i", 3};  // 3 channels
+
         Input<int> rad{"radius"};
         Input<float> epsilon{"epsilon"};
         Output<Halide::Buffer<uint8_t>> output{"output", 2};
@@ -37,37 +38,41 @@ namespace {
         const bool FastGF = true;
 
         void generate() {
-            assert(guidance.channels() == 3);
+            assert(guidance_i.channels() == 3);
 
-            const int k_size = 10;
+            const int k_size = 20;
 
             if (FastGF) {
-                downsample_2x(input_bounded, Halide::BoundaryConditions::repeat_edge(input));
-                downsample_2x(guidance_bounded, Halide::BoundaryConditions::repeat_edge(guidance));
+                downsample_2x(p_bounded, Halide::BoundaryConditions::repeat_edge(input_p));
+                downsample_2x(i_bounded, Halide::BoundaryConditions::repeat_edge(guidance_i));
             } else {
-                input_bounded(x, y) = f32(Halide::BoundaryConditions::repeat_edge(input)(x, y));
-                guidance_bounded(x, y, c) = f32(Halide::BoundaryConditions::repeat_edge(guidance)(x, y, c));
+                p_bounded(x, y) = f32(Halide::BoundaryConditions::repeat_edge(input_p)(x, y));
+                i_bounded(x, y, c) = f32(Halide::BoundaryConditions::repeat_edge(guidance_i)(x, y, c));
             }
+            // Algorithm 1 Guided Filter [1]
+            // Step 1
+            box_blur<float, float>(mean_p, p_bounded, k_size);
+            box_blur<float, float>(mean_i, i_bounded, k_size);
 
-            box_blur<float, float>(mean_input, input_bounded, k_size);
-            box_blur<float, float>(mean_guidance, guidance_bounded, k_size);
+            calc_channel_corr(channel_corr_i, i_bounded, i_bounded, 3);
+            box_blur<float, float>(corr_i, channel_corr_i, k_size);
 
-            box_blur<float, float>(mean_ig, lambda(x, y, c, f32(input_bounded(x, y)) * guidance_bounded(x, y, c)), k_size);
+            box_blur<float, float>(corr_ip, lambda(x, y, c, f32(p_bounded(x, y)) * i_bounded(x, y, c)), k_size);
 
-            calc_channel_cov(cov_g, guidance_bounded, guidance_bounded, 3);
-            box_blur<float, float>(mean_cov_g, cov_g, k_size);
+            // Step 2
+            calc_channel_corr(channel_corr_mean_i, mean_i, mean_i, 3);
+            var_i(x, y, c) = corr_i(x, y, c) - channel_corr_mean_i(x, y, c);
 
-            calc_channel_cov(cov_mg, mean_guidance, mean_guidance, 3);
+            cov_ip(x, y, c) = f32(corr_ip(x, y, c)) - f32(mean_p(x, y)) * mean_i(x, y, c);
 
-            var_i(x, y, c) = mean_cov_g(x, y, c) - cov_mg(x, y, c);
+            // Step 3
+            // a
+            sym_mat_inv_3x3(inv_var_i, var_i, epsilon);
+            sym_mat_mul(a, inv_var_i, cov_ip);
+            // b
+            b(x, y) = f32(mean_p(x, y)) - a(x, y, 0) * mean_i(x, y, 0) - a(x, y, 1) * mean_i(x, y, 1) - a(x, y, 2) * mean_i(x, y, 2);
 
-            inv_var_i(x, y, c) = sym_mat_inv_3x3(var_i, epsilon)(x, y, c);
-
-            cov_ip(x, y, c) = f32(mean_ig(x, y, c)) - f32(mean_input(x, y)) * mean_guidance(x, y, c);
-
-            a(x, y, c) = sym_mat_mul(inv_var_i, cov_ip)(x, y, c);
-            b(x, y) = f32(mean_input(x, y)) - a(x, y, 0) * mean_guidance(x, y, 0) - a(x, y, 1) * mean_guidance(x, y, 1) - a(x, y, 2) * mean_guidance(x, y, 2);
-
+            // Step 4
             box_blur<float, float>(mean_a, a, k_size);
             box_blur<float, float>(mean_b, b, k_size);
 
@@ -79,10 +84,11 @@ namespace {
                 umean_b = mean_b;
             }
 
+            // Step 5
             output(x, y) = u8_sat(
-                umean_a(x, y, 0) * f32(guidance(x, y, 0)) +
-                umean_a(x, y, 1) * f32(guidance(x, y, 1)) +
-                umean_a(x, y, 2) * f32(guidance(x, y, 2)) +
+                umean_a(x, y, 0) * f32(guidance_i(x, y, 0)) +
+                umean_a(x, y, 1) * f32(guidance_i(x, y, 1)) +
+                umean_a(x, y, 2) * f32(guidance_i(x, y, 2)) +
                 umean_b(x, y)
             );
         }
@@ -95,14 +101,14 @@ namespace {
                 const auto tile_h = 16;
                 Var xo("xo"), yo("yo"), xi("xi"), yi("yi"), tile("tile");
 
-                input_bounded.compute_root();
-                guidance_bounded.compute_root();
+                p_bounded.compute_root();
+                i_bounded.compute_root();
 
-                mean_input.compute_root();
-                mean_guidance.compute_root().bound(c, 0, 3);
-                mean_ig.compute_root();
+                mean_p.compute_root();
+                mean_i.compute_root().bound(c, 0, 3);
+                corr_ip.compute_root();
                 mean_ii.compute_root();
-                mean_cov_g.compute_root();
+                corr_i.compute_root();
                 cov_ip.compute_root();
                 var_i.compute_root();
                 inv_var_i.compute_root();
@@ -118,19 +124,25 @@ namespace {
                 }
                 output.tile(x, y, xo, yo, xi, yi, tile_w, tile_h).fuse(xo, yo, tile).vectorize(xi, tile_w).unroll(yi);
 
-                for (Func* func : {&cov_g, &mean_cov_g, &cov_mg, &input_bounded, &guidance_bounded, &mean_input, &mean_guidance, &mean_ig, &mean_ii, &cov_ip, &var_i, &inv_var_i, &a, &b, &mean_a, &mean_b}) {
-                    const std::string dumpFileName = "func_dumps/" + func->name();
-                    if (func->outputs() > 1) {
-                        std::cerr << "Can't dump " << func->name() << std::endl;
-                    } else {
-                        std::cerr << "Making function " << func->name() << " dump: " << dumpFileName << std::endl;
-                        func->debug_to_file(dumpFileName);
+                const bool make_dumps = false;
+                if (make_dumps) {
+                    const auto functions_to_dump = {&channel_corr_i, &corr_i, &channel_corr_mean_i, &p_bounded, &i_bounded,
+                                                    &mean_p, &mean_i, &corr_ip, &mean_ii, &cov_ip, &var_i,
+                                                    &inv_var_i, &a, &b, &mean_a, &mean_b};
+                    for (Func *func : functions_to_dump) {
+                        const std::string dumpFileName = "func_dumps/" + func->name();
+                        if (func->outputs() > 1) {
+                            std::cerr << "Can't dump " << func->name() << std::endl;
+                        } else {
+                            std::cerr << "Making function " << func->name() << " dump: " << dumpFileName << std::endl;
+                            func->debug_to_file(dumpFileName);
+                        }
                     }
                 }
             }
         }
     private:
-        Func sym_mat_mul(const Halide::Func& m, const Halide::Func& v) {
+        void sym_mat_mul(const Halide::Func& dest, const Halide::Func& m, const Halide::Func& v) {
             Expr ma = f32(m(x, y, 0));
             Expr mb = f32(m(x, y, 1));
             Expr mc = f32(m(x, y, 2));
@@ -142,17 +154,15 @@ namespace {
             Expr vy = v(x, y, 1);
             Expr vz = v(x, y, 2);
 
-            Func result(m.name() + "_" + v.name() + "_sym_mat_mul");
-            result(x, y, c) = select(
+            dest(x, y, c) = select(
                 c == 0, ma * vx + mb * vy + mc * vz,
                 c == 1, mb * vx + md * vy + me * vz,
                 c == 2, mc * vx + me * vy + mf * vz,
                 0
             );
-            return result;
         }
 
-        Func sym_mat_inv_3x3(const Halide::Func& m, Expr epsilon) {
+        void sym_mat_inv_3x3(Halide::Func& dest, const Halide::Func& m, Expr epsilon) {
             /* Derivation:
              * >>> from sympy import *
              * >>> a, b, c, d, e, f = symbols('a b c d e f')
@@ -171,8 +181,7 @@ namespace {
             Func det(m.name() + "_det");
             det(x, y) = (ma * md * mf - ma * me * me - mb * mb * mf + 2 * mb * mc * me - mc * mc * md);
 
-            Func result(m.name() + "_inv");
-            result(x, y, c) = select(
+            dest(x, y, c) = select(
                  c == 0, (md * mf - me * me) / det(x, y),
                  c == 1, -(mb * mf - mc * me) / det(x, y),
                  c == 2,  (mb * me - mc * md) / det(x, y),
@@ -181,11 +190,10 @@ namespace {
                  c == 5, (ma * md - mb * mb) / det(x, y),
                  0
             );
-            return result;
         }
 
         template<class TResult=unsigned char, class TSum=signed short>
-        Halide::Func& box_blur(Halide::Func& out, const Halide::Func& in, const int k_radius = 5) const {
+        void box_blur(Halide::Func& dest, const Halide::Func& in, const int k_radius = 5) const {
             // TODO: implement O(1) filtering
             assert(k_size > 0 && k_size % 2 == 1);
             const auto normalization = 2 * k_radius + 1;
@@ -196,61 +204,60 @@ namespace {
 
             const auto sum_cast = [&](const Expr& e, const std::string& sfx = "") {
                 if constexpr (std::is_integral_v<TSum>) {
-                    return fast_integer_divide(sum(Halide::saturating_cast<TSum>(e), out.name() + "_sum" + sfx), normalization);
+                    return fast_integer_divide(sum(Halide::saturating_cast<TSum>(e), dest.name() + "_sum" + sfx), normalization);
                 } else {
-                    return sum(Halide::cast<TSum>(e), out.name() + "_sum" + sfx) / normalization;
+                    return sum(Halide::cast<TSum>(e), dest.name() + "_sum" + sfx) / normalization;
                 }
             };
 
             if (in.dimensions() == 2) {
                 blur_x(x, y) = sum_cast(in(x + rx.x, y), "_x");
-                out(x, y) = Halide::saturating_cast<TResult>(sum_cast(blur_x(x, y + ry.x)));
+                dest(x, y) = Halide::saturating_cast<TResult>(sum_cast(blur_x(x, y + ry.x)));
             } else if (in.dimensions() == 3) {
                 blur_x(x, y, c) = sum_cast(in(x + rx.x, y, c), "_x");
-                out(x, y, c) = Halide::saturating_cast<TResult>(sum_cast(blur_x(x, y + ry.x, c)));
+                dest(x, y, c) = Halide::saturating_cast<TResult>(sum_cast(blur_x(x, y + ry.x, c)));
             } else {
                  throw std::runtime_error("Wrong number of dimensions in box_blur");
             }
-            blur_x.compute_root(); //at(filtered, x);
-            return out;
+            //blur_x.compute_root(); //at(dest, x); //at(filtered, x);
         }
 
 
         // Downsample with a 1 3 3 1 filter
-        void downsample_2x(Func& out, const Func& in) {
+        void downsample_2x(Func& dest, const Func& in) {
             Func downx(in.name() + "_downx");
             if (in.dimensions() == 2) {
                 downx(x, y) = f32(f32(in(2 * x - 1, y)) + 3.0f * (f32(in(2 * x, y)) + in(2 * x + 1, y)) + f32(in(2 * x + 2, y))) / 8.0f;
-                out(x, y) = (downx(x, 2 * y - 1) + 3.0f * (downx(x, 2 * y) + downx(x, 2 * y + 1)) + downx(x, 2 * y + 2)) / 8.0f;
+                dest(x, y) = (downx(x, 2 * y - 1) + 3.0f * (downx(x, 2 * y) + downx(x, 2 * y + 1)) + downx(x, 2 * y + 2)) / 8.0f;
             } else if (in.dimensions() == 3) {
                 downx(x, y, c) = f32(f32(in(2 * x - 1, y, c)) + 3.0f * (f32(in(2 * x, y, c)) + in(2 * x + 1, y, c)) + f32(in(2 * x + 2, y, c))) / 8.0f;
-                out(x, y, c) = (downx(x, 2 * y - 1, c) + 3.0f * (downx(x, 2 * y, c) + downx(x, 2 * y + 1, c)) + downx(x, 2 * y + 2, c)) / 8.0f;
+                dest(x, y, c) = (downx(x, 2 * y - 1, c) + 3.0f * (downx(x, 2 * y, c) + downx(x, 2 * y + 1, c)) + downx(x, 2 * y + 2, c)) / 8.0f;
             }
         }
 
         // Upsample using bilinear interpolation
-        void upsample_2x(Func& out, const Func& f) {
+        void upsample_2x(Func& dest, const Func& f) {
             using Halide::_;
             Func upx(f.name() + "_upx");
             if (f.dimensions() == 2) {
                 upx(x, y) = 0.25f * f((x / 2) - 1 + 2 * (x % 2), y) + 0.75f * f(x / 2, y);
-                out(x, y) = 0.25f * upx(x, (y / 2) - 1 + 2 * (y % 2)) + 0.75f * upx(x, y / 2);
+                dest(x, y) = 0.25f * upx(x, (y / 2) - 1 + 2 * (y % 2)) + 0.75f * upx(x, y / 2);
             } else if (f.dimensions() == 3) {
                 upx(x, y, c) = 0.25f * f((x / 2) - 1 + 2 * (x % 2), y, c) + 0.75f * f(x / 2, y, c);
-                out(x, y, c) = 0.25f * upx(x, (y / 2) - 1 + 2 * (y % 2), c) + 0.75f * upx(x, y / 2, c);
+                dest(x, y, c) = 0.25f * upx(x, (y / 2) - 1 + 2 * (y % 2), c) + 0.75f * upx(x, y / 2, c);
             }
         }
 
     private:
-        Func input_bounded = Func{"input_bounded"};
-        Func guidance_bounded = Func{"guidance_bounded"};
-        Func mean_input = Func{"mean_input"};
-        Func mean_guidance = Func{"mean_guidance"};
-        Func mean_ig = Func{"mean_ig"};
+        Func p_bounded = Func{"p_bounded"};
+        Func i_bounded = Func{"i_bounded"};
+        Func mean_p = Func{"mean_p"};
+        Func mean_i = Func{"mean_i"};
+        Func corr_ip = Func{"corr_ip"};
         Func mean_ii = Func{"mean_ii"};
-        Func cov_g = Func{"cov_g"};
-        Func mean_cov_g = Func{"mean_cov_g"};
-        Func cov_mg = Func{"cov_mg"};
+        Func channel_corr_i = Func{"channel_corr_i"};
+        Func corr_i = Func{"corr_i"};
+        Func channel_corr_mean_i = Func{"channel_corr_mean_i"};
 
         Func cov_ip = Func{"cov_ip"};
         Func var_i = Func{"var_i"};
@@ -264,7 +271,6 @@ namespace {
         Var x = {"x"};
         Var y = {"y"};
         Var c = {"c"};
-        Var c2 = {"c2"};
     };
 
 }  // namespace
